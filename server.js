20565f0f8e4485e8b9b35dc77e060f8b4ff30daa
Alyssa Cervantes (she/her) <112597248+alyssaleanne98@users.noreply.github.com>
2022-10-14 17:03:53 -0700

    Tip Calculator express application created

--- server.js
+++ server.js
@@ -1,23 +1,38 @@
 // Greetings 
 
-const express = require("express")
+// const express = require("express")
 
 // called the express function, and storing the return in app
-const app = express()
+// const app = express()
 
 // the root route 
+// app.get("/", (req, res) => {
+//     // console log "hello stranger"
+//     res.send("hi there!");
+// });
+
+// add route 
+// app.get("/greeting/:Alyssa?", (req, res) => {
+//     // console.log("You did it!")
+//     // if (req.params.name) {
+//         res.send(
+//             "<h> Alyssa! It's so great to see you! <h/>"
+//         );
+// });
+
+// app.listen(3000)
+
+// Tip calculator 
+
+const express = require("express")
+const app = express()
+
 app.get("/", (req, res) => {
-    // console log "hello stranger"
     res.send("hi there!");
 });
 
-// add route 
-app.get("/greeting/:Alyssa?", (req, res) => {
-    // console.log("You did it!")
-    // if (req.params.name) {
-        res.send(
-            "<h> Alyssa! It's so great to see you! <h/>"
-        );
-});
-
+// multiply route 
+app.get("/tip/:total/:tipPercentage", (req, res) => {
+    res.send(`${ parseInt(req.params.tipPercentage) * parseInt(req.params.total) / 100 }`)
+}); 
 app.listen(3000)
