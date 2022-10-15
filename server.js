11ebeee2767fc49d2b3244631ac6f5bed4a29745
Alyssa Cervantes (she/her) <112597248+alyssaleanne98@users.noreply.github.com>
2022-10-14 19:19:13 -0700

    Magic 8 Ball express application created

--- server.js
+++ server.js
@@ -24,15 +24,37 @@
 
 // Tip calculator 
 
+// const express = require("express")
+// const app = express()
+
+// app.get("/", (req, res) => {
+//     res.send("hi there!");
+// });
+
+// // multiply route 
+// // : means that it is looking for the "total" and "tipPercentage"
+// app.get("/tip/:total/:tipPercentage", (req, res) => {
+//     res.send(`${ parseInt(req.params.tipPercentage) * parseInt(req.params.total) / 100 }`)
+// }); 
+// app.listen(3000)
+
+// Magic 8 Ball 
+
 const express = require("express")
 const app = express()
 
 app.get("/", (req, res) => {
-    res.send("hi there!");
+    res.send("hi there");
 });
 
-// multiply route 
-app.get("/tip/:total/:tipPercentage", (req, res) => {
-    res.send(`${ parseInt(req.params.tipPercentage) * parseInt(req.params.total) / 100 }`)
-}); 
+const answer = [
+    "It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"
+]
+const randomAnswer = answer[Math.floor(Math.random() * answer.length)];
+// we can't use spaces in url, so we use %20 to express a space in the url. 
+app.get("/magic/Will%20I%20Be%20A%20Millionaire", (req, res) => {
+
+    res.send("Will I be a millionaire?" + randomAnswer );
+});
+
 app.listen(3000)
